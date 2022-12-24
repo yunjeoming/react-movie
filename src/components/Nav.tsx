8c1aab8b9f90cdc0816d8179fdd0ea9ed5bfddba
yunjeoming <yunjeoming@gmail.com>
2022-12-24 22:05:35 +0900

    fix Nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "./Nav.css";
 
 const Nav = () => {
   const [show, setShow] = useState(false);
+  const [searchValue, setSearchValue] = useState("");
+  const navigate = useNavigate();
 
   useEffect(() => {
     window.addEventListener("scroll", () => {
@@ -16,14 +19,33 @@ const Nav = () => {
     return window.removeEventListener("click", () => {});
   }, []);
 
+  const handleChange = (value: string) => {
+    setSearchValue(value);
+    navigate(`/search?q=${value}`);
+  };
+
   return (
     <nav className={`nav ${show && "nav__black"}`}>
       <img
         alt="Netflix logo"
         src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
         className="nav__logo"
-        onClick={() => window.location.reload()}
+        onClick={() => navigate('/')}
       />
+      <div className="nav__input-wrapper">
+        <svg
+          className="nav__search-icon"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 512 512"
+        >
+          <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352c79.5 0 144-64.5 144-144s-64.5-144-144-144S64 128.5 64 208s64.5 144 144 144z" />
+        </svg>
+        <input
+          className="nav__input"
+          value={searchValue}
+          onChange={(e) => handleChange(e.target.value)}
+        />
+      </div>
       <img
         alt="User logo"
         src="https://www.hotelbooqi.com/wp-content/uploads/2021/12/128-1280406_view-user-icon-png-user-circle-icon-png.png"
