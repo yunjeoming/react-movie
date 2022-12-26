26e06bdc4ed93607822d55d0c3b1624b046d84b3
yunjeoming <yunjeoming@gmail.com>
2022-12-26 20:12:25 +0900

    fix url and readme

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="react-netflix-clone">
       <App />
     </BrowserRouter>
   </React.StrictMode>
